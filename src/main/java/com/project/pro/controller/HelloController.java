37f93fe54e4c4c77a9403b373620fd543298c5ae
iee <horocolow@gmail.com>
2022-10-05 23:15:22 +0900

    10월5일

--- src/main/java/com/project/pro/controller/HelloController.java
+++ src/main/java/com/project/pro/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/chat/")
     public String index() {
         return "index";
     }
